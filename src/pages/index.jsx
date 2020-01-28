bfd856135b4fcf5fea82f8fd0de032b460f28e52
Sarthak Chowdhary <sarthak.chow@gmail.com>
2020-01-28 13:36:26 +0530

    spring cleaning

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+// import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+  </Layout>
+)
+
+export default IndexPage
