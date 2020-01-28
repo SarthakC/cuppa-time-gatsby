69435672a0e149a3fc36dde35d1484a9e6ece348
Sarthak Chowdhary <sarthak.chow@gmail.com>
2020-01-28 13:42:59 +0530

    [added] react-bootstrap and react-icons

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,14 +1,8 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 // import { useStaticQuery, graphql } from "gatsby"
 
+import "bootstrap/dist/css/bootstrap.min.css"
 import "./layout.css"
 
 const Layout = ({ children }) => <>{children}</>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 // import { Link } from "gatsby"
 
+import { FaGulp } from "react-icons/fa"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -9,6 +11,7 @@ const IndexPage = () => (
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
+    <FaGulp />
   </Layout>
 )
 
