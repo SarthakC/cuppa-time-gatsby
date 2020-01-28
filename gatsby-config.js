472614d9f8241005bfc70df5892a0fb25aed606f
Sarthak Chowdhary <sarthak.chow@gmail.com>
2020-01-29 00:33:40 +0530

    [connected] to contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `your_space_id`,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
