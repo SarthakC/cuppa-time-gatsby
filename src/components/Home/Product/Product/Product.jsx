8decc89f6cf39d6213011156db204b40ef9cd07c
Sarthak Chowdhary <sarthak.chow@gmail.com>
2020-01-30 22:57:45 +0530

    [added] product card

--- src/components/Home/Product/Product/Product.jsx
+++ src/components/Home/Product/Product/Product.jsx
@@ -1,7 +1,24 @@
 import React from "react"
+import Img from "gatsby-image"
+import { Col, Card, Button } from "react-bootstrap"
 
-const Product = () => {
-  return <div>Hello from product</div>
+const Product = ({ product: { image, title, price } }) => {
+  return (
+    <Col xs={10} sm={8} md={6} lg={4} className="mx-auto my-3">
+      <Card style={{ minHeight: "100%" }}>
+        <Img fluid={image.fluid} as={Card.Img} variant="top" />
+        <Card.Body className="text-center">
+          <Card.Text>
+            <h6>{title}</h6>
+            <h6>{price}</h6>
+            <Button className="btn-yellow mt-3 text-capitalize" variant="light">
+              add to cart
+            </Button>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  )
 }
 
 export default Product
