c5ee9cd00c4f456aa9ce7f45a463ccc363aadbfb
Sarthak Chowdhary <sarthak.chow@gmail.com>
2020-01-31 00:31:34 +0530

    [added] snipkart

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,13 @@ module.exports = {
       },
     },
     {
+      resolve: "gatsby-plugin-snipcart",
+      options: {
+        apiKey: process.env.SNIPCART_KEY,
+        autoPop: true,
+      },
+    },
+    {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
